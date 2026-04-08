91413890b1112f43c461f6dd5df942ad99aef4ea
Matia Paki <matia.m.paki@gmail.com>
2026-04-08 19:17:04 -0400

    made the corner posts of the boxing ring

--- js/boxingRing.js
+++ js/boxingRing.js
@@ -51,7 +51,7 @@ controls.target.set(0, 1, 0);
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight('red', 1.5);
+const directionalLight = new THREE.DirectionalLight('white', 1.5);
 directionalLight.position.set(0, 10, 0);
 directionalLight.castShadow = true;
 scene.add(directionalLight);
@@ -61,17 +61,62 @@ scene.add(directionalLight);
 const floorMaterial = new THREE.MeshStandardMaterial({
     color: 'beige'
 });
-
 floorMaterial.roughness = 0.9;
+
+const groundMaterial = new THREE.MeshStandardMaterial({
+    color: "maroon"
+});
+groundMaterial.roughness = 0.95;
+
+const postMaterial = new THREE.MeshStandardMaterial({
+    color: "blue"
+});
+
 
 // floor
 
+//ring floor
 const floor = new THREE.Mesh(
     new THREE.BoxGeometry(8, 0.15, 8),
     floorMaterial
 );
 floor.receiveShadow = true;
 scene.add(floor);
+
+// floor of the room itself
+const ground = new THREE.Mesh(
+    new THREE.PlaneGeometry(60,60),
+    groundMaterial
+);
+
+ground.rotation.x = -Math.PI * 0.5;
+ground.position.y = -0.68;
+ground.receiveShadow = true;
+scene.add(ground);
+
+// corner posts
+
+const postGeometry = new THREE.CylinderGeometry(0.07, 0.07, 2, 12);
+
+const post1 = new THREE.Mesh(postGeometry, postMaterial);
+    post1.position.set(-4, 1, -4);
+    post1.castShadow = true;
+    scene.add(post1);
+
+const post2 = new THREE.Mesh(postGeometry, postMaterial);
+    post2.position.set(4, 1, -4);
+    post2.castShadow = true;
+    scene.add(post2);
+
+const post3 = new THREE.Mesh(postGeometry, postMaterial);
+    post3.position.set(-4, 1, 4);
+    post3.castShadow = true;
+    scene.add(post3);
+
+const post4 = new THREE.Mesh(postGeometry, postMaterial);
+    post4.position.set(4, 1, 4);
+    post4.castShadow = true;
+    scene.add(post4);
 
 // animate
 
